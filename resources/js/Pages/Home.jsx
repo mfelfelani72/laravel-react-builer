ec361a0c0d9826fe4b92db33dadbdf9d892d720f
mohammad <mfelfelani72@gmail.com>
2025-03-24 12:32:51 +0000

    edit

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Head } from "@inertiajs/react";
 import { useEffect } from "react";
 
 const Home = ({ posts }) => {
@@ -6,10 +7,40 @@ const Home = ({ posts }) => {
     }, []);
     return (
         <>
-            <div className="py-2 font-bold text-teal-500 flex flex-row w-full justify-center">Home</div>
+            <Head>
+                <title>asdsa</title>
+                <meta charset="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,
+                   initial-scale=1,
+                   maximum-scale=1"
+                />
+                <meta
+                    name="description"
+                    content="A Computer Science portal for geeks. 
+         It contains well written, well thought 
+         and well explained computer science and 
+         programming articles, quizzes and practice/competitive
+         programming/company interview Questions."
+                />
+                <meta
+                    http-equiv="Content-Type"
+                    content="text/html; charset=utf-8"
+                />
+            </Head>
+            <div className="py-2 font-bold text-teal-500 flex flex-row w-full justify-center">
+                Home
+            </div>
             <div>
                 {posts?.map((post, index) => (
                     <div key={index} className="p-4 border-b font-bold">
+                        <div className="text-sm text-slate-600">
+                            <span>Posted on: </span>
+                            <span>
+                                {new Date(post.created_at).toLocaleTimeString()}{" "}
+                            </span>
+                        </div>
                         <p>{post?.body}</p>
                     </div>
                 ))}
