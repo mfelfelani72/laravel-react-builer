239c72ac52a95afa317c724196828b32d7103912
mohammad <mfelfelani72@gmail.com>
2025-03-24 14:22:10 +0000

    edit

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -3,10 +3,9 @@ import { Link } from "@inertiajs/react";
 const Layout = ({ children }) => {
     return (
         <main>
-            <header className="flex flex-row justify-between h-12 items-center px-4 text-amber-50 bg-pink-500">
+            <header className="flex flex-row justify-between h-20 items-center px-24 text-amber-50 bg-cyan-950">
                 <Link preserveScroll href="/">Home</Link>
-                <Link href="/about">About</Link>
-                <Link href="/contact">Contact</Link>
+                <Link href="/contact">Create</Link>
             </header>
             <article>{children}</article>
         </main>
